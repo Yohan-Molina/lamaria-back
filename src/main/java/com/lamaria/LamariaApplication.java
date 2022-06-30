a5bec56d214d265a79500487c0ff122f121891f7
Yohan-Molina <jhohan.molina4321@gmail.com>
2022-06-29 21:03:29 -0500

    Se crea backend para Dulces Artesanales La Maria

---
+++ src/main/java/com/lamaria/LamariaApplication.java
@@ -0,0 +1,13 @@
+package com.lamaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LamariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LamariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lamaria/LamariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LamariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
